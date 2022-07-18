091bea13cd2c4dbf7a199a2620c74403fa928c2c
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-07-18 21:54:35 +0200

    Category Page

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,21 +1,22 @@
+import { Link } from 'react-router-dom';
 import './category-item.styles.scss';
 
 const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category;
 
   return (
-    <div className='category-container'>
+    <Link className='category-item-container' to={`shop/${title}`}>
       <div
         className='background-image'
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className='category-body-container'>
+      <div className='body'>
         <h2>{title}</h2>
         <p>Shop now</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
