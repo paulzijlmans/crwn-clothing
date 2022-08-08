28d8a35c4a11a31c19c4b485887de93cc988451a
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-08-08 12:13:51 +0200

    Add Spinner

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -14,4 +14,9 @@ export const selectCategoriesMap = createSelector(
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {}))
+    }, {}));
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
+)
