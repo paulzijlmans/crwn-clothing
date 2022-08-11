750f5fad7d5814d361ce71234165efc055a79535
paulzijlmans <paulzijlmans@hotmail.com>
2022-08-11 11:38:15 +0200

    Stripe (#4)
    * Stripe Setup
    
    * Set Up Payment Form
    
    * Serverless Function Payment Intent
    
    * Finish Stripe Payment
    
    * Tidy Up UI

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,5 +1,6 @@
 import {
   BaseButton,
+  ButtonSpinner,
   GoogleSignInButton,
   InvertedButton,
 } from './button.styles.jsx';
@@ -17,9 +18,13 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...buttonProps }) => {
+const Button = ({ children, buttonType, isLoading, ...buttonProps }) => {
   const CustomButton = getButton(buttonType);
-  return <CustomButton {...buttonProps}>{children}</CustomButton>;
+  return (
+    <CustomButton disabled={isLoading} {...buttonProps}>
+      {isLoading ? <ButtonSpinner /> : children}
+    </CustomButton>
+  );
 };
 
 export default Button;
